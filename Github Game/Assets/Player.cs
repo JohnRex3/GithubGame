54f7f824382c4aa7062aae5e5215073e90b4fbdf
RN274 <ghprogramming111@gmail.com>
2021-01-29 21:31:09 -0800

    Update 7

--- Github Game/Assets/Player.cs
+++ Github Game/Assets/Player.cs
@@ -6,19 +6,27 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
     [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+
+    // State
+    bool isAlive = true;
+
+    // Cached component references
 
     Rigidbody2D myRigidBody;
+    Animator myAnimator;
 
     //Use this for initialization
     void Start ()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     //Update is called once per frame
-    void Update()
-    {
+    void Update () {
         Run();
+        Jump();
         FlipSprite();
     }
 
@@ -27,8 +35,20 @@ public class Player : MonoBehaviour
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // value is between -1 to +1
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+        print(playerVelocity);
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", true);
     }
     
+    private void Jump()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myRigidBody.velocity += jumpVelocityToAdd;
+        }
+    }
     private void FlipSprite()
     {
         bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
